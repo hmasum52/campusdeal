2f67c9b9e219ecd5c2b709a1c4347f0545d90a05
hasan masum <hasanmasum1852@gmail.com>
2023-11-06 18:39:27 +0600

    6-viewbinding-and-top-level-fragments

---
+++ app/src/main/java/com/codervai/campusdeal/screen/OnBoardingFragment.java
@@ -0,0 +1,38 @@
+package com.codervai.campusdeal.screen;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.codervai.campusdeal.R;
+import com.codervai.campusdeal.databinding.FragmentOnBoardingBinding;
+
+public class OnBoardingFragment extends Fragment {
+
+    private FragmentOnBoardingBinding mVB;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mVB = FragmentOnBoardingBinding.inflate(inflater, container, false);
+        return mVB.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mVB.getStartBtn.setOnClickListener(view1 -> {
+           // Toast.makeText(getContext(), "Get start", Toast.LENGTH_SHORT).show();
+            NavHostFragment.findNavController(this)
+                    .navigate(R.id.action_onBoardingFragment_to_homeFragment);
+        });
+    }
+}
