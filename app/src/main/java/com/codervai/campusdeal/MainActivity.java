c85e3a8e316f15b238c06b50b5f2b4a555b685b1
hasan masum <hasanmasum1852@gmail.com>
2023-11-10 01:12:23 +0600

    7-adding-BottomNavigationView

--- app/src/main/java/com/codervai/campusdeal/MainActivity.java
+++ app/src/main/java/com/codervai/campusdeal/MainActivity.java
@@ -1,14 +1,26 @@
 package com.codervai.campusdeal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.codervai.campusdeal.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mVB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mVB = ActivityMainBinding.inflate(super.getLayoutInflater());
+        setContentView(mVB.getRoot());
+
+        // set up bottom navigation bar with navhost fragments nav controller.
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(mVB.bottomNavigation, navController);
     }
 }
