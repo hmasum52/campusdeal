ca8b865f4edf6ff701cd7ee758c3ace3a21f6640
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-11-10 02:32:14 +0600

    8-hiding bottom navigation from non top level fragments

--- app/src/main/java/com/codervai/campusdeal/MainActivity.java
+++ app/src/main/java/com/codervai/campusdeal/MainActivity.java
@@ -1,15 +1,23 @@
 package com.codervai.campusdeal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavArgument;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.codervai.campusdeal.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavController.OnDestinationChangedListener {
 
     private ActivityMainBinding mVB;
 
@@ -22,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
         // set up bottom navigation bar with navhost fragments nav controller.
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(mVB.bottomNavigation, navController);
+
+        navController.addOnDestinationChangedListener(this);
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController navController,
+                                     @NonNull NavDestination navDestination,
+                                     @Nullable Bundle bundle) {
+        Map<String, NavArgument> args = navDestination.getArguments();
+
+        boolean showBottomNav = args.containsKey("showBottomNav")
+                && Objects.equals(Objects.requireNonNull(args.get("showBottomNav")).getDefaultValue(), true);
+
+        if(showBottomNav){
+            mVB.bottomNavigation.setVisibility(View.VISIBLE);
+        }else {
+            mVB.bottomNavigation.setVisibility(View.GONE);
+        }
     }
 }
