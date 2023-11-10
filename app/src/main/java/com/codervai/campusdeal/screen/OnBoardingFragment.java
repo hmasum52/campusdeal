c0dddb220414ff750101ee056fdd56dcc7818bde
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-11-10 15:38:30 +0600

    12-completing firebase authentication

--- app/src/main/java/com/codervai/campusdeal/screen/OnBoardingFragment.java
+++ app/src/main/java/com/codervai/campusdeal/screen/OnBoardingFragment.java
@@ -1,12 +1,18 @@
 package com.codervai.campusdeal.screen;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +21,41 @@ import android.widget.Toast;
 import com.codervai.campusdeal.MainActivity;
 import com.codervai.campusdeal.R;
 import com.codervai.campusdeal.databinding.FragmentOnBoardingBinding;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class OnBoardingFragment extends Fragment {
 
     private FragmentOnBoardingBinding mVB;
+
+    // https://developer.android.com/training/basics/intents/result
+    // https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#configuration
+    ActivityResultLauncher<Intent> signInIntentLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    //IdpResponse response = result.getIdpResponse();
+                    if(result.getResultCode() == RESULT_OK){
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        Log.d("After Sing in: ", user.getDisplayName());
+                    }else{
+                        //response.getError().getErrorCode();
+                    }
+                }
+            }
+    );
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,12 +67,40 @@ public class OnBoardingFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.fragment_login)
+                .setGoogleButtonId(R.id.google_sign_in_btn)
+                .setEmailButtonId(R.id.email_sing_in_btn)
+                .build();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.Theme_CampusDeal)
+                .setAvailableProviders(providers)
+                .setAuthMethodPickerLayout(customLayout)
+                .build();
+
+
         mVB.getStartBtn.setOnClickListener(view1 -> {
-           // Toast.makeText(getContext(), "Get start", Toast.LENGTH_SHORT).show();
-            NavHostFragment.findNavController(this)
-                    .navigate(R.id.action_onBoardingFragment_to_loginFragment);
-            //MainActivity.navigateToStartDestination(getActivity(),
-            //        R.id.action_onBoardingFragment_to_homeFragment, R.id.homeFragment);
+            if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+                // https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#sign-out
+                AuthUI.getInstance()
+                        .signOut(requireContext())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getContext(), "Signed out", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                return;
+            }
+            signInIntentLauncher.launch(signInIntent);
         });
     }
 }
